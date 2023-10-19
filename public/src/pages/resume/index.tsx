4c11c9fc016e6afdf1fa644931c49af0fc971e6c
Curtis <cjones2362@outlook.com>
2023-10-18 22:49:00 -0400

    Built resume template

--- public/src/pages/resume/index.tsx
+++ public/src/pages/resume/index.tsx
@@ -17,12 +17,69 @@ export const Resume = () => {
         {
             title: 'Web Developer',
             location: 'GiftCash, Ancaster, Ontario',
-            when: 'January 2018 – August 2023',
+            when: 'January 2020 – August 2023',
             points: [
                 'Worked with both front and back end applications to allow for the faster processing of gift cards and providing a better customer experience'
             ]
         }
     ]
+
+
+    const education = [
+        {
+            title: 'Bachelor of Technology – Automation Engineering Technology  ',
+            location: 'McMaster University, Hamilton, Ontario',
+            // TODO confirm this
+            when: 'September 2018 – April 2023',
+            points: [
+                'Awarded Bachelor of Technology Degree, Chemical Engineering Technology Advanced Diploma, and a Business Management Certificate'
+            ]
+        }
+    ]
+
+    const projects = [
+            'This website'
+    ]
+
+    const buildUl = (list: string[]) => {
+        return (
+            <ul>
+                {list.map((item) => (
+                    <li>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
+    // TODO move the type and use for above vars
+    const buildMulti = (list: {
+        title: string,
+        location: string,
+        when: string,
+        points: string[]
+    }[]) => {
+
+        return ( list.map((item) => (
+            <div className='multi'>
+                <h3>{item.title}</h3>
+                <Grid container
+                    justifyContent="flex-end"
+                    alignItems="center"
+                >
+                    <Grid item xs={6} className='left-align'>
+                        <p className='work-location'> {item.location} </p>
+                    </Grid>
+                    <Grid item xs={6} className='right-align'>
+                        <p className='work-location'> {item.when} </p>
+                    </Grid>
+                </Grid>
+                {buildUl(item.points)}
+            </div>
+        )) )
+    }
+
     return (
         <div className='resume'>
             <h1> Curtis Jones </h1>
@@ -43,47 +100,30 @@ export const Resume = () => {
 
             <h2> Profesional Summary </h2>
             <div className='left-align'>
-                <ul>
-                    {profesionalSummary.map((item) => (
-                        <li>
-                            {item}
-                        </li>
-                    ))}
-                </ul>
+                {buildUl(profesionalSummary)}
             </div>
 
             <div className='line' />
 
             <h2> Work Experiance </h2>
             <div className='left-align'>
-                {
-                    workExperiance.map((job) => (
-                        <div className='job'>
-                            <h3>{job.title}</h3>
-                            <Grid container
-                                justifyContent="flex-end"
-                                alignItems="center"
-                            >
-                                <Grid item xs={6} className='left-align'>
-                                    <p className='work-location'> {job.location} </p>
-                                </Grid>
-                                <Grid item xs={6} className='right-align'>
-                                    <p className='work-location'> {job.when} </p>
-                                </Grid>
-                            </Grid>
-                            <ul>
-                                {
-                                    job.points.map((point) => (
-                                        <li>
-                                            {point}
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                    ))
-                }
+                {buildMulti(workExperiance)}
             </div>
+
+            <div className='line' />
+
+            <h2> Education </h2>
+            <div className='left-align'>
+                {buildMulti(education)}
+            </div>
+
+            <div className='line' />
+
+            <h2> Projects </h2>
+            <div className='left-align'>
+                {buildUl(projects)}
+            </div>
+            <p> References Available Upon Request </p>
         </div>
     );
 }
