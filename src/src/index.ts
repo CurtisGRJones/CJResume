3f39507690e289cd6b80cf7d7cd50fc75701106e
Curtis <cjones2362@outlook.com>
2023-10-19 14:25:09 -0400

    Made service a better class

--- src/src/index.ts
+++ src/src/index.ts
@@ -1,24 +1,26 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import config from './config';
 import { Services } from './services';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const app: Express = express();
-app.use(bodyParser.json());
+try {
+  const app: Express = express();
+  app.use(bodyParser.json());
 
-console.log(process.env.NODE_ENV )
+  if (process.env.NODE_ENV === 'development') {
+    app.use(cors({
+      origin: 'http://localhost:3000'
+    }));
+  }
 
-if( process.env.NODE_ENV === 'development' ) {
-  app.use(cors({
-    origin: 'http://localhost:3000'
-  }));
+  const services = new Services()
+
+  app.use('/api', services.router);
+
+  app.listen(config.PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${config.PORT}/api/ as ${process.env.NODE_ENV} server`);
+  });
+} catch (e) {
+  console.error(`[server]: Failed to start server due to error\n${e}`)
 }
-
-const services = new Services()
-
-app.use('/api/', services.router);
-
-app.listen(config.PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${config.PORT}/api/`);
-});
--- src/src/services/index.ts
+++ src/src/services/index.ts
@@ -5,42 +5,42 @@ import { middleware as cmsMiddleware } from './cms';
 
 export class Services {
 
-    status: { [serviceName: string]: boolean }  = {}
+    status: { [serviceName: string]: boolean } = {}
+    public readonly router: Router
 
-    router = (): Router => {
+    constructor() {
         const services: {
             name: string,
             path?: string
             router: Router
         }[] = [
-            calculatorMiddleware,
-            configMiddleware,
-            cmsMiddleware
-        ]
-    
-        const router = Router()
-    
-        services.forEach( (middleware) => {
+                calculatorMiddleware,
+                configMiddleware,
+                cmsMiddleware
+            ]
+        
+        this.router = Router()
+
+        this.router.get('/', (req: Request, res: Response, next: NextFunction) => {
+            // TODO make this check serices as they are used
+            res.send({
+                running: true,
+                services: this.status
+            })
+            next();
+        });
+
+        services.forEach((middleware) => {
             let success = true
             const path = middleware.path || `/${middleware.name}/`
-            // TODO add more error logging
             try {
-                router.use(path, middleware.router)
-            } catch {
+                this.router.use(path, middleware.router)
+            } catch(e) {
+                console.error(`${middleware.name} failed with error:\n${e}`)
                 success = false
             }
             this.status[middleware.name] = success
-        } )
-
-        router.get('/api/', (req: Request, res: Response, next: NextFunction) => {
-            // TODO make this check serices as they are used
-            res.send({
-              running: true,
-              services: this.status
-            })
-            next();
-          });
-        return router
-    } 
+        })
+    }
 }
 
