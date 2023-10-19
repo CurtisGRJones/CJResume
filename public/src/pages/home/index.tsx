0932f051e8f40c7dbc5618d8f9486c3e240357ae
Curtis <cjones2362@outlook.com>
2023-10-18 21:24:18 -0400

    Updated langing page

--- public/src/pages/home/index.tsx
+++ public/src/pages/home/index.tsx
@@ -1,21 +1,43 @@
 import './home.css'
 
 export const Home = () => {
+    const knownTech = [
+        'TypeScript',
+        'JavaScript',
+        'Express',
+        'Node',
+        'Yarn',
+        'React',
+        'MySQL',
+        'MongoDB',
+        'HTML',
+        'CSS',
+        'Git',
+    ]
     return (
         <div className='home'>
-            <h1> Curtis Jones </h1>
-            <h2> Web Developer </h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum erat elit, sollicitudin eu vehicula vitae, mollis ac dui. Ut non luctus magna. In ut luctus dui. Sed dictum sed quam non ullamcorper. Quisque placerat ex sit amet fermentum elementum. Mauris semper quam et nulla ullamcorper, ut lobortis dolor mattis. Sed convallis, purus id elementum aliquet, purus leo congue turpis, a sagittis ex sem et velit. Fusce sit amet leo lacus. Morbi non posuere arcu. Vivamus bibendum quam vel mi faucibus, id euismod mauris varius. Nulla iaculis tempor ex, et auctor ipsum consequat ac. Etiam eu arcu sed orci hendrerit sagittis at tempus turpis. Sed id elit non quam varius condimentum. Vestibulum varius euismod felis et iaculis. Donec vitae tortor quis lectus condimentum posuere a in arcu.
-
-Proin maximus quis ante vel posuere. Vestibulum a venenatis ex. Vestibulum nec metus consectetur, facilisis nisl eu, tristique ligula. Sed accumsan tortor sed condimentum sagittis. Vestibulum pharetra viverra sapien, non placerat sapien sagittis iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc sollicitudin ornare ante, quis iaculis leo consequat a. Morbi a ultrices mi, nec lacinia orci. Pellentesque lacinia purus quis convallis fermentum. Vestibulum dui est, pharetra id urna suscipit, vulputate posuere risus.
-
-Nam vitae volutpat mi. Pellentesque ullamcorper lorem nec mi volutpat accumsan. Vivamus quis rutrum ante. In vel diam neque. In maximus pretium orci ac venenatis. Integer malesuada malesuada ante ac interdum. Nunc a est pharetra, condimentum felis eu, tincidunt nisi. Quisque mollis viverra risus ac sollicitudin. Pellentesque eget ipsum nisi. Sed elementum ornare erat sed volutpat.
-
-Proin pretium, nisl et fermentum congue, eros ipsum vestibulum est, eu dignissim sapien augue eget augue. Ut elementum vulputate maximus. Integer tincidunt nibh quis massa dictum tincidunt. Integer tincidunt sodales augue ultricies accumsan. Sed in sodales dolor. Suspendisse vitae iaculis velit. Morbi eu metus enim. Proin varius odio non leo efficitur tempus. Vestibulum rutrum, sem ultricies dapibus auctor, orci nunc scelerisque lorem, id venenatis nisi odio eget tellus.
-
-Proin id tortor augue. Integer erat arcu, interdum maximus dapibus sit amet, interdum a massa. Phasellus rutrum scelerisque rhoncus. Integer facilisis porttitor mauris. Maecenas hendrerit mauris non tempor fringilla. Cras in felis eget quam luctus gravida a ac eros. Curabitur malesuada lacus at efficitur vulputate. Aenean hendrerit ligula urna, ut tincidunt odio accumsan a. Proin sit amet hendrerit massa. In viverra ultrices sapien, quis dictum nulla sagittis nec.
-            </p>
+            <div className='home-content'>
+                <h1> 
+                    Hello and Welcome, <br/><br/>
+                    My name is Curtis Jones, <br/><br/>
+                    Software Developement is my trade <br/><br/>
+                    Feel free to stick around and <br />
+                    look at some of my projects
+                </h1>
+            </div>
+            <div className='known-tech'>
+                <h1> 
+                    Technologies used to build this website <br/><br/>
+                    <table>
+                        {knownTech.map( (tech) => (
+                            <tr>
+                                {tech}
+                            </tr>
+                        ) )}
+                    </table>
+                </h1>
+                <h3> And more to come </h3>
+            </div>
         </div>
     );
 }
