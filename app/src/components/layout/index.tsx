0c29cd105321525753eed2d722f0ec505e24fdba
Curtis <cjones2362@outlook.com>
2023-10-23 00:01:33 -0400

    Updated to try to fix mobile rendering issue

--- app/src/components/layout/index.tsx
+++ app/src/components/layout/index.tsx
@@ -17,7 +17,6 @@ export const Layout = () => {
     window.addEventListener('scroll', () => {
       console.log(`.${styles['top-bar']}`);
       (document.querySelector(`.${styles['top-bar']}`) as HTMLElement).style.top = `-${document.documentElement.scrollTop}px`;
-      //document.documentElement.style.setProperty('--scroll-position-x',`${document.documentElement.scrollLeft} px`);
       console.log('scroll: ', `${document.documentElement.scrollTop} px`);
     })
   }, [])
@@ -31,9 +30,15 @@ export const Layout = () => {
       <div className={[styles['page'], 'preload'].join(' ')}>
         <div className={[styles['navigation-bar'], ( showNav || permNav ) ? styles['show'] : styles['hide']].join(' ')}>
           <NavigationBar />
-          {!permNav && <div className={styles['show-nav']} onClick={handleShowPress}> 
-            <p className={styles['arrows']}>{ showNav ? '<<' : '>>' }</p>
-          </div>}
+          {!permNav && ( showNav ?
+            <div className={[styles['show-nav'], styles['left']].join(' ')} onClick={handleShowPress}> 
+              <p className={styles['arrows']}> {'<<'} </p>
+            </div>
+            :
+            <div className={[styles['show-nav'], styles['right']].join(' ')} onClick={handleShowPress}> 
+              <p className={styles['arrows']}> {'>>'} </p>
+            </div>
+          )}
         </div>
         <div className={styles['scroll-page']}> 
           <div className={styles['top-bar']}>
