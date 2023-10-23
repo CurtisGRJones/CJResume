c46ac4f694305f0689e442fd3866bfad2967c19d
Curtis <cjones2362@outlook.com>
2023-10-22 21:35:40 -0400

    Updated to add ability to show nav bar

--- app/src/components/layout/index.tsx
+++ app/src/components/layout/index.tsx
@@ -1,16 +1,33 @@
 import { Outlet } from "react-router-dom";
 import { NavigationBar } from "..";
 import { TopBar } from "../topBar";
+import { useEffect, useState } from "react";
 
 import styles from './layout.module.css'
 
-
 export const Layout = () => {
+  const [showNav, setShowNav] = useState(false)
+  const [permNav, setPermNav] = useState(false)
+
+  useEffect(() => {
+    setPermNav(window.innerWidth >= 750)
+    window.addEventListener('resize', () => {
+      setPermNav(window.innerWidth >= 750)
+    })
+  }, [])
+
+  const handleShowPress = () => {
+    setShowNav(!showNav)
+  }
+
   return (
     <>
       <div className={[styles['page'], 'preload'].join(' ')}>
-        <div className={styles['navigation-bar']}>
+        <div className={[styles['navigation-bar'], ( showNav || permNav ) ? styles['show'] : styles['hide']].join(' ')}>
           <NavigationBar />
+          {!permNav && <div className={styles['show-nav']} onClick={handleShowPress}> 
+            <p className={styles['arrows']}>{ showNav ? '<<' : '>>' }</p>
+          </div>}
         </div>
         <div className={styles['scroll-page']}> 
           <div className={styles['top-bar']}>
